d24dcd9f23e4aa7d66c6a46d18ee0758fc8d976e
james chalmers <james.chalmers184@gmail.com>
2022-07-24 21:18:09 +0100

    finished chapter 11

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <TodoForm :about="about" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from "vue";
+import TodoForm from "./components/TodoForm.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    TodoForm,
+  },
+  setup() {
+    const about = ref({
+      title: "Vue 3",
+      subTitle: "TypeScript demo",
+    });
+    return {
+      about,
+    };
   },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap/dist/css/bootstrap.css";
 </style>
